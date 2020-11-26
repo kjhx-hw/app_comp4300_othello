24ad733d7ba3b577058091d0f33dd8cd4e884b92
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2020-11-25 22:06:25 -0600

    cleanup until Riley is done with minimax function

--- src/ai/Glados.js
+++ src/ai/Glados.js
@@ -32,49 +32,4 @@ class Glados {
     // Tile has to be of type TILE.EMPTY | TILE.WHITE | TILE.BLACK
     
   }
-
-  //parityHeuristic = MaxPlayerCoins - MinPlayerCoins;
-  //cornerheuristic = (maxcapturedCorners + maxPotentialCorners + 
-    //((maxCapturedCorners + minCapturedCorners) * unlikely corners)) - 
-    //(mincapturedCorners + minPotentialCorners + 
-    //((maxCapturedCorners + minCapturedCorners) * unlikely corners))
-  //captured Corner = 4
-  //potential Corners = 3
-  //unlikely corners = 1
-
-  minimax(position, maxDepth, alpha, beta, maxPlayer) {
-    if (maxDepth == 0 || 0/*GAMEOVER*/) {
-        //return EVALUATIONOFPOSITION;
-    }
-    if (maxPlayer) {
-        maxEvaluation = -infinity;
-        if (1/*Possible moves == 0*/) {
-          //minimax(thisGameBoard, depth - 1, alpha, beta, false);
-        } else {
-          /*FOR EACH CHILD OF POSITION {
-              evaluation = minimax(CHILD, depth - 1, alpha, beta, false);
-              maxEvaluation = max(maxEvaluation, evaluation);
-              alpha = max(alpha, eval);
-              if (beta <= alpha) {
-                  break;
-              }
-          }*/
-        }
-        return maxEval;
-    } else {
-        minEval = infinity;
-        if (1/*Possible moves == 0*/) {
-          //minimax(thisGameBoard, depth - 1, alpha, beta, true);
-        } else {
-          /*FOR EACH CHILD OF POSITION {
-              evaluation = minimax(CHILD, depth - 1, alpha, beta, true);
-              minEvaluation = min(minEval, eval);
-              beta = min(beta, eval)
-              if (beta <= alpha) {
-                  break;
-              }*/
-        }
-        return minEval;
-      }
-    }
 }
