b01b07d9c7f6cab5e0f6707ac4d540fa3a0d4e0a
Kyle Huggins <5430818+kjhx@users.noreply.github.com>
2020-11-25 23:46:47 -0600

    Add stability, tile getters, and comments

--- src/ai/Glados.js
+++ src/ai/Glados.js
@@ -1,18 +1,33 @@
 const TILE = {
   'EMPTY': 0,
-  'WHITE': 1,
-  'BLACK': 2
+  'BLACK': 1,
+  'WHITE': 2
+}
+
+const STATUS = {
+  'STABLE': 0,
+  'NEARSTAB': 1,
+  'UNSTABLE': 2
 }
 
 class Glados {
-  // Create empty 2d array
-  static gameboard = [[], [], [], [], [], [], [], []];
+  static gameboard = this.getBigArray();
+  static stabTracker = this.getBigArray();
+  static playerTiles = {
+    black: 0,
+    white: 0
+  }
   
-  static initGameboard() {
-    console.log('Initializing game board');
+  static getBigArray() {
+    return [[], [], [], [], [], [], [], []];
+  }
+
+  static initBoards() {
+    console.log('Initializing game & stability boards');
     for (let i = 0; i <= 7; i++) {
       for (let j = 0; j <= 7; j++) {
         this.gameboard[i][j] = TILE.EMPTY;
+        this.stabTracker[i][j] = STATUS.STABLE;
       }
     }
 
@@ -20,16 +35,49 @@ class Glados {
   }
 
   static visGameboard() {
-    console.log('Current board status:');
+    console.log('Current gameboard status:');
     console.table(this.gameboard);
   }
 
+  static visStabTracker() {
+    console.log('Current stability status:');
+    console.table(this.stabTracker);
+  }
+
   static setGameboard(board) {
-    console.log('Setting gameboard to new value');
+    // Input `board` is a 2D array
+    console.log('Setting gameboard to new values');
+  }
+
+  static updatePlayerTiles() {
+    this.playerTiles.white = 0;
+    this.playerTiles.black = 0;
+
+    for (let i = 0; i < this.gameboard.length; i++) {
+      for (let j = 0; j < this.gameboard[i].length; j++) {
+        if (this.gameboard[i][j] === TILE.WHITE) {
+          this.playerTiles.white++;
+        } else if (this.gameboard[i][j] === TILE.BLACK) {
+          this.playerTiles.black++;
+        }
+      }
+    }
   }
 
   static getPlayerTiles(tile) {
-    // Tile has to be of type TILE.EMPTY | TILE.WHITE | TILE.BLACK
+    // Input `tile` has to be of type TILE.BLACK | TILE.WHITE
+    switch (tile) {
+      case TILE.BLACK:
+        return this.playerTiles.black;
+        break;
+
+      case TILE.WHITE:
+        return this.playerTiles.white;
+        break;
     
+      default:
+        return -1;
+        break;
+    }
   }
 }
